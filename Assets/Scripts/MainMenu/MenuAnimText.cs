16567f841296bc78458e645e23a057d2be82f4f6
RoboticWater <jakebritti@gmail.com>
2018-10-01 23:28:48 -0400

    added fancy menu + statue head characters

---
+++ Assets/Scripts/MainMenu/MenuAnimText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuAnimText : MonoBehaviour {
+	public string[] states;
+	public Text text;
+	public float switchTime = 0.25f;
+	float timer;
+	int state;
+
+	void Update () {
+		if (timer <= 0)
+		{
+			timer = switchTime;
+			state = (state + 1) % states.Length;
+			text.text = states[state];
+		}
+		timer -= Time.deltaTime;
+	}
+}
