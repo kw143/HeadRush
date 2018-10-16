b25eb86129251b0bc9559f7ab6e19da799659fcd
sjoplin3 <sjoplin3@gatech.edu>
2018-10-16 12:53:06 -0400

    Added a way to spawn enemies, a player indicator on minimap, and fixed turning for the main player vehicle

---
+++ Assets/Scripts/EnemyStarter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStarter : MonoBehaviour {
+    //This is the Array to house all the enemies that will "Spawn" when crossed
+    public GameObject[] enemiesToSpawn = new GameObject[3];
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //The player script calls this when it hits the trigger
+    public void Activate() {
+        foreach (GameObject enemy in enemiesToSpawn) {
+            enemy.SetActive(true);
+        }
+    }
+}
