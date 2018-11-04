69ef15c82397824ba57932faa0291ea153097c81
cmullins77 <mullins.cassie@gmail.com>
2018-11-04 13:24:50 -0500

    Adding the 3 Effects
    *Enemy Death Glitch
    >Post Effect Masks
    >Glitch Effect Shader
    >Enemy Model and Textures
    *Enemy Particle Explosion
    >Particle Materials
    >Particle Prefab
    *Player Death
    > Unity Post Processing
    > Post Processing Profile
    > Script to Control Post Processing upon Death

---
+++ Assets/Effects/EnemyDeathParticleExplosion/EnemyDeath2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath2 : MonoBehaviour {
+
+    public GameObject particles;
+    public bool triggerDeath;
+    public Renderer childCube;
+
+	// Use this for initialization
+	void Start () {
+        particles.SetActive(false);
+        triggerDeath = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(triggerDeath) {
+            triggerDeath = false;
+            GetComponent<Renderer>().enabled = false;
+            childCube.enabled = false;
+            particles.SetActive(true);
+        }
+	}
+}
